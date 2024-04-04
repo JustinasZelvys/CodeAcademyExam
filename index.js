d3bea7ca75ccb8aeca6785f995de25d5123075b1
JustinasZelvys <zelvys.justinas@gmail.com>
2024-04-04 19:19:34 +0300

    adding mobile version 50% done

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-function toggleDropdown() {
-    var x = document.getElementById("header-main-nav");
-    var dropdown = document.getElementsByClassName("dropdown")[0];
-    dropdown.classList.toggle("show");
-    if (x.className === "header-main-nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "header-main-nav";
-    }
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.icon')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+window.onload=function(){
+
+    // burger animation
+    
+    let burger = document.getElementById("burger");
+    let menu = document.querySelector(".header-main-nav");
+    
+    function burgerClick()  {
+        if (burger.classList.contains('active')) {
+            burger.classList.remove('active');
+            menu.classList.remove('translated');
+            }
+            else {
+            burger.classList.add('active');
+            menu.classList.add('translated');
+            }
         }
-      }
+    burger.addEventListener('click', burgerClick) ;
     }
-  }
-  
